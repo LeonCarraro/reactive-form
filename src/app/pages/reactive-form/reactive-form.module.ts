0cbacc28b63a2d2dc0efa86bfc3b905c3c0b1b1d
Leonardo Carraro <lcarraro.oliveira@gmail.com>
2021-09-26 14:33:53 -0300

    update primeng from v8 to v12

--- src/app/pages/reactive-form/reactive-form.module.ts
+++ src/app/pages/reactive-form/reactive-form.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 
 import { ReactiveFormComponent } from './reactive-form.component';
 
 @NgModule({
   declarations: [ReactiveFormComponent],
-  imports: [CommonModule, ReactiveFormsModule, ButtonModule, InputTextModule],
+  imports: [CommonModule, ReactiveFormsModule, ButtonModule, CardModule, InputTextModule],
   exports: [ReactiveFormComponent],
   providers: [],
 })
